cf9e2cdee178761324fcdca2716faf7a8c69400f
tolu <olabiyitolu@gmail.com>
2024-07-01 13:06:46 +0100

    basic web server

---
+++ api.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, jsonify
+import requests
+import geocoder
+
+app = Flask(__name__)
+
+API_KEY = '27b597283caa2674af2979ade8fe7156'
+
+@app.route('/app/hello')
+def hello():
+    name = request.args.get('visitor name')
+    ip = request.remote_addr
+
+    geolocation = geocoder.ip(ip)
+    location = geolocation.city
+    # location = 'Lagos'
+
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={location}&appid={API_KEY}"
+    response = requests.get(url)
+    data = response.json()
+    temperature = data['main']['temp']
+
+    response = {
+        # 'name': name,
+        'client ip': ip,
+        'location': location,
+        'greetings' : f"Hello, {name}!, the temperature is {temperature} degrees Celcius in {location}" 
+    }
+    return jsonify(response)
+
+if __name__ == "__main__":
+    app.run(debug = True)
