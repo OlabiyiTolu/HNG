6489efd0ba6dc025936e472b5f58b4861704ccde
tolu <olabiyitolu@gmail.com>
2024-07-02 12:57:13 +0100

    visitor_name update

--- app.py
+++ app.py
@@ -1,32 +1,33 @@
 from flask import Flask, request, jsonify
 import requests
 import geocoder
-# from mykey import key
+
 
 app = Flask(__name__)
 
-API_KEY = '27b597283caa2674af2979ade8fe7156'
+API_KEY = '27b597283caa2674af2979ade8fe7156'  # Replace with your actual API key
 
 @app.route('/api/hello')
 def hello():
     name = request.args.get('visitor_name')
     # if name is None:
     #     name = "Guest"
-    # name = 'Tolu'
-    ip = request.remote_addr
+    # name = Tolu
+
+    # Get client IP from headers
+    ip = request.headers.get('X-Forwarded-For', request.remote_addr)  # Check for X-Forwarded-For header
 
     geolocation = geocoder.ip(ip)
     location = geolocation.city
-    # location = 'Lagos'
+
 
     url = f"https://api.openweathermap.org/data/2.5/weather?q={location}&appid={API_KEY}"
     response = requests.get(url)
     data = response.json()
     temperature = data['main']['temp']
     temperature = (temperature - 273.15)
-
+    
     response = {
-        # 'name': name,
         'client ip': ip,
         'location': location,
         'greetings' : f"Hello, {name}!, the temperature is {int(temperature)} degrees Celcius in {location}" 
@@ -34,4 +35,4 @@ def hello():
     return jsonify(response)
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(debug = True) 
